9e36dcfb0a47f58bdd11ab6b412872ded798406a
Chetan Deshpande <chetan.deshpande@protonmail.com>
2022-02-14 14:29:36 +0530

    added email verification using nodemailer

--- controller/userController.js
+++ controller/userController.js
@@ -1,45 +1,44 @@
 const Users = require("../model/userModel");
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
-const SendmailTransport = require("nodemailer/lib/sendmail-transport");
+ const sendMail = require("../sendMail");
 
-exports.register = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    // if (!name || !email || !password) {
-    //   res.json({ msg: "Fill up all the fields" });
-    // }
+ exports.register = async (req, res) => {
+   try {
+     const { name, email, password } = req.body;
+     // if (!name || !email || !password) {
+     //   res.json({ msg: "Fill up all the fields" });
+     // }
 
-    const user = await Users.findOne({email})
-    if(user) {
-        return res.status(400).json({msg:"User Already Exits"})
+     const user = await Users.findOne({ email });
+     if (user) {
+       return res.status(400).json({ msg: "User Already Exits" });
+     }
 
-    }
-  
-    const hashedPassword =await bcrypt.hash(password,12)
-    const newUser = {
-        name,email,password:hashedPassword
-    }
-    const activateToken = createToken(newUser)
-    console.log(activateToken)
-    const url = `${process.env.CLIENT_URL}/user/activate/${activateToken}`
-    // sendMail(email,url)
-    res.status(200).json({msg:"Registeration Success !!Please activate email"})
-  } catch (error) {
-    return res.status(500).json({ msg: "error" });
-  }
-};
-const createToken=(payload)=>{
-    return jwt.sign(payload,process.env.JWT_ACC_ACTIVATE,{expiresIn:'5m'})
-}
+     const hashedPassword = await bcrypt.hash(password, 12);
+     const newUser = {
+       name,
+       email,
+       password: hashedPassword,
+     };
+     const activateToken = createToken(newUser);
+     //  console.log(activateToken);
+     const url = `${process.env.CLIENT_URL}/user/activate/${activateToken}`;
+     sendMail(email, url);
+     res
+       .status(200)
+       .json({ msg: "Registeration Success !!Please activate email" });
+   } catch (error) {
+     return res.status(500).json({ msg: "error" });
+   }
+ };
 
-const sendMail = ()=>{}
+ const createToken = (payload) => {
+   return jwt.sign(payload, process.env.JWT_ACC_ACTIVATE, { expiresIn: "5m" });
+ };
 
-
-
-// const validateEmail = (email) => {
-
-// };
+ 
+//  const validateEmail = (email) => {};
 
 exports.login = async () => {};
 exports.logout = async () => {};
---
+++ sendMail.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+const nodeMailgun = require("nodemailer-mailgun-transport");
+const mailgun = require("mailgun-js");
+
+const sendMail = (email, url) => {
+  //   console.log(email);
+  const auth = {
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+
+  //   let transporter = nodemailer.createTestAccount(nodeMailgun(auth));
+  const sendFrom = "noreply@demo.com";
+  let userEmail = email;
+  const confirmUrl = url;
+
+  const mailOptions = {
+    from: sendFrom,
+    to: userEmail,
+    subject: "Hello",
+    html: `${confirmUrl}`,
+  };
+
+  mg.messages().send(mailOptions, function (error, body) {
+    console.log(body);
+  });
+  //   transporter.sendMail(mailOptions, (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log("msg send");
+  //     }
+  //   });
+};
+
+module.exports = sendMail;
